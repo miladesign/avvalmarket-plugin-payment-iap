a380a42a0a4ccd6edb1bc9c1dd55ff2c8f3fcaa2
miladesign <rezagah.milad@gmail.com>
2016-08-02 12:50:11 +0430

    Plugin Created

---
+++ www/iap.js
@@ -0,0 +1,49 @@
+module.exports = {
+	// SetUp IAP
+	setUp: function(androidApplicationLicenseKey) {
+		cordova.exec(
+			function (result) {
+				console.log("Initialize Success. By Miladesign.ir");
+			}, 
+			null,
+			'IAP',
+			'setUp',			
+			[androidApplicationLicenseKey]
+		); 
+	},
+	
+	// requestStoreListing
+	requestStoreListing: function (productIds, successCallback, failureCallback) {
+		if (!productIds || productIds.length == 0) {
+			return successCallback({});
+		}
+		if (Object.prototype.toString.call(productIds) === '[object String]') {
+			productIds = [ productIds ];
+		}
+		cordova.exec(successCallback, failureCallback, "IAP", "requestStoreListing", [ productIds ]);	
+	},
+	
+	// purchaseProduct
+	purchaseProduct: function(productId, successCallback, failureCallback) {
+		if (!productId) {
+			return failureCallback("noProductId");
+		}
+		cordova.exec(successCallback, failureCallback, "IAP", "purchaseProduct", [ productId ]);		
+	},	
+	
+	// consumeProduct
+	consumeProduct: function (receipt, successCallback, failureCallback) {
+		if (!receipt || receipt.length == 0) {
+			return successCallback();
+		}
+		if (Object.prototype.toString.call(receipt) === '[object String]') {
+			receipt = [ receipt ];
+		}
+		cordova.exec(successCallback, failureCallback, "IAP", "consumeProduct", [ receipt ]);
+	},	
+	
+	// restorePurchases
+	restorePurchases: function (successCallback, failureCallback) {
+		cordova.exec(successCallback, failureCallback, "IAP", "restorePurchases", []);
+	}
+};
